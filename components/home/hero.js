bee5ed7f4b100eaacf5ac6b7d4ac68dc57672a3f
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-12-30 03:07:01 +0530

    Hero section Updated

--- components/home/hero.js
+++ components/home/hero.js
@@ -37,9 +37,15 @@ export default function Hero({ locale, CTALocale }) {
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ delay: 0.4, type: 'spring', stiffness: 100, damping: 10 }}
 				>
-					<h1 className='font-bold text-5xl md:text-7xl bg-gradient-to-r from-base-content from-50% to-[#9c9c9c] text-center bg-clip-text text-transparent !leading-[1.25em] mb-5'>
-						{locale.h1}
-					</h1>
+					<div className='font-bold text-5xl flex flex-col md:text-7xl bg-gradient-to-r from-base-content from-70% to-[#7124ff] text-center bg-clip-text text-transparent !leading-[1.25em] mb-5'>
+					{/* <div className='flex flex-col text-base-content/80'> */}
+				{/* <h2 className='ml-3 font-bold leading-5'>EXPO ESSENTIALS</h2> */}
+				{/* <p className='ml-3 text-xs'>BOOTH CONSTRUCTION</p> */}
+				{/* </div> */}
+				<div> EXPO ESSENTIALS</div>
+				<div className='text-2xl'>BOOTH CONSTRUCTION</div>
+					</div>
+					
 				</motion.div>
 
 				<motion.div
@@ -50,10 +56,14 @@ export default function Hero({ locale, CTALocale }) {
 						duration: 1,
 					}}
 				>
-					<h2 className='w-full md:w-10/12 mx-auto text-xl md:text-2xl text-base-content/80 md:text-center mb-5 md:mb-10'>{locale.h2}</h2>
+					<h2 className='w-full font-[550] md:w-10/12 mx-auto text-xl md:text-2xl text-base-content/90 md:text-center mb-5 md:mb-10'>
+					
+					{/* {locale.h2} */}
+					Elevate Your Brand with Our Custom Expo Booths—Where First Impressions Count.
+					</h2>
 
-					<div className='w-full md:w-8/12 mx-auto flex flex-col md:flex-row md:items-center justify-between gap-y-5'>
-						<HeroIcons />
+					<div className='w-full md:w-8/12 mx-auto flex flex-col  md:flex-row md:items-center justify-center gap-y-5'>
+						{/* <HeroIcons /> */}
 
 						<div className='flex flex-col md:flex-row gap-2'>
 							<a
@@ -86,7 +96,7 @@ export default function Hero({ locale, CTALocale }) {
 					<Image
 						width={1024}
 						height={600}
-						src={'/og.png'}
+						src={'/hero3.jpg'}
 						className='hidden md:flex w-full -mt-10'
 						alt='app demo'
 					/>
@@ -94,47 +104,47 @@ export default function Hero({ locale, CTALocale }) {
 			</section>
 
 			<div className='absolute w-[100%] left-[0] top-[10%] md:top-[20%] h-[300px]'>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					id='patternId'
-					width='100%'
-					height='100%'
-				>
-					<defs>
-						<pattern
-							id='a'
-							patternUnits='userSpaceOnUse'
-							width='20'
-							height='20'
-							patternTransform='scale(3) rotate(0)'
-						>
-							<rect
-								x='0'
-								y='0'
-								width='100%'
-								height='100%'
-								fill='hsla(0, 0%, 100%, 0)'
-							></rect>
-							<path
-								d='M 10,-2.55e-7 V 20 Z M -1.1677362e-8,10 H 20 Z'
-								strokeWidth='0.5'
-								className='stroke-base-content/50'
-								fill='none'
-							></path>
-						</pattern>
-					</defs>
-					<rect
-						width='800%'
-						height='800%'
-						transform='translate(0,0)'
-						fill='url(#a)'
-					></rect>
-				</svg>
-				<div className='bg-gradient-to-b from-base-100  from-20% to-transparent absolute inset-0 '></div>
-				<div className='bg-gradient-to-l from-base-100  from-1% to-transparent to-30% absolute inset-0'></div>
-				<div className='bg-gradient-to-r from-base-100  from-1% to-transparent to-30% absolute inset-0'></div>
-				<div className='bg-gradient-to-t from-base-100  from-1% to-transparent to-30% absolute inset-0'></div>
-			</div>
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        id='patternId'
+        width='100%'
+        height='100%'
+    >
+        <defs>
+            <pattern
+                id='a'
+                patternUnits='userSpaceOnUse'
+                width='20'
+                height='20'
+                patternTransform='scale(3) rotate(0)'
+            >
+                <rect
+                    x='0'
+                    y='0'
+                    width='100%'
+                    height='100%'
+                    fill='hsla(0, 0%, 100%, 0)'
+                ></rect>
+                <path
+                    d='M 10,0 V 20 Z M 0,10 H 20 Z'
+                    strokeWidth='0.15'
+                    stroke='#7124ff'  
+                    fill='none'
+                ></path>
+            </pattern>
+        </defs>
+        <rect
+            width='800%'
+            height='800%'
+            transform='translate(0,0)'
+            fill='url(#a)'
+        ></rect>
+    </svg>
+    <div className='bg-gradient-to-b from-base-100 from-20% to-transparent absolute inset-0'></div>
+    <div className='bg-gradient-to-l from-base-100 from-1% to-transparent to-30% absolute inset-0'></div>
+    <div className='bg-gradient-to-r from-base-100 from-1% to-transparent to-30% absolute inset-0'></div>
+    <div className='bg-gradient-to-t from-base-100 from-1% to-transparent to-30% absolute inset-0'></div>
+</div>
 		</>
 	);
 }
