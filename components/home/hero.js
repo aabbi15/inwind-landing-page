681744aeaeae6d94d522aa59cbd133917fe54b51
Abhishek Abbi <abhishekabbi.work@gmail.com>
2025-02-10 03:10:36 +0530

    hero

--- components/home/hero.js
+++ components/home/hero.js
@@ -14,9 +14,9 @@ export default function Hero({ locale, CTALocale }) {
 
 	useEffect(() => {
 		const handleScroll = () => {
-			const maxTilt = 45; // 最大倾斜角度
+			const maxTilt = 60; 
 			const scrollY = window.scrollY;
-			const tiltValue = Math.max(maxTilt - scrollY / 8, 0); // 根据滚动值调整
+			const tiltValue = Math.max(maxTilt - scrollY / 8, 0); 
 			setTilt(tiltValue);
 			setDuration(0.3);
 		};
@@ -39,11 +39,8 @@ export default function Hero({ locale, CTALocale }) {
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ delay: 0.4, type: 'spring', stiffness: 100, damping: 10 }}
 				>
-					<div className='font-bold text-5xl flex flex-col md:text-7xl bg-gradient-to-r from-base-content from-70% to-[#7124ff] text-center bg-clip-text text-transparent !leading-[1.25em] mb-5'>
-					{/* <div className='flex flex-col text-base-content/80'> */}
-				{/* <h2 className='ml-3 font-bold leading-5'>EXPO ESSENTIALS</h2> */}
-				{/* <p className='ml-3 text-xs'>BOOTH CONSTRUCTION</p> */}
-				{/* </div> */}
+					<div className='font-bold text-4xl flex flex-col md:text-[66px] text-center  !leading-[1.25em] mb-5'>
+				
 				<div> EXPO ESSENTIALS</div>
 				<div className='text-2xl'>BOOTH CONSTRUCTION</div>
 					</div>
@@ -58,42 +55,20 @@ export default function Hero({ locale, CTALocale }) {
 						duration: 1,
 					}}
 				>
-					<h2 className='w-full font-[550] md:w-10/12 mx-auto text-xl md:text-2xl text-base-content/90 md:text-center mb-5 md:mb-10'>
 					
-					{/* {locale.h2} */}
-					Elevate Your Brand with Our Custom Expo Booths—Where First Impressions Count.
-					</h2>
-
-					<div className='w-full md:w-8/12 mx-auto flex flex-col  md:flex-row md:items-center justify-center gap-y-5'>
+					<div className='w-full md:w-full mx-auto flex flex-col  md:flex-row md:items-center justify-center gap-y-5'>
 						{/* <HeroIcons /> */}
 
-						<div className='flex flex-col md:flex-row gap-2'>
-							{/* <a
-								title='get source code'
-								className='btn btn-sm md:btn-md btn-base border-none hover:ring-1 ring-base-content text-3xl text-base-100 hover:text-base-content bg-base-content hover:bg-base-100 rounded-full'
-								href='https://github.com/huglemon/inwind-landing-page'
-							>
-								<SiGithub />
-								<BsPhoneFill/>
-								{CTALocale.btn1}
-								Book a Call
-							</a> */}
+						<div className=''>
+							
 							<CTAButton/>
-							{/* <a
-								title='get source code'
-								className='btn btn-sm md:btn-md btn-base text-xl rounded-full'
-								href='https://huglemon.com/blog/i-open-sourced-a-saas-landing-page'
-							>
-								<IoDocumentText /> 
-								{/* {CTALocale.btn2} 
-
-								Book a Call
-							</a> */}
+							
 						</div>
 					</div>
 				</motion.div>
+
 				<motion.div
-					initial={{ opacity: 0, y: 100, rotateX: '0deg' }}
+					initial={{ opacity: 0, y: 100, rotateX: '45deg' }}
 					animate={{ opacity: 1, y: 0, rotateX: `${tilt}deg` }}
 					transition={{
 						delay: duration == 0.8 ? 0.4 : 0,
@@ -102,17 +77,17 @@ export default function Hero({ locale, CTALocale }) {
 					className='w-full justify-center items-center flex relative'
 				>
 					<Image
-						width={800}
-						height={900}
+						width={900}
+						height={600}
 						src={'/guidea.png'}
-						className='hidden md:flex w-[100%] -mt-10 rounded-lg '
+						className='hidden md:flex  -mt-10 rounded-lg '
 						alt='app demo'
 					/>
 
 				</motion.div>
 			</section>
 
-			<div className='absolute w-[100%] left-[0] top-[10%] md:top-[20%] h-[300px]'>
+			<div className='absolute w-[100%] left-[0] top-[50%] md:top-[10%] h-[300px]'>
     <svg
         xmlns='http://www.w3.org/2000/svg'
         id='patternId'
