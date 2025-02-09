2729d0e7be189b88bcff9cb12a8d0aa0172f343d
Abhishek Abbi <abhishekabbi.work@gmail.com>
2025-02-10 03:44:46 +0530

    fix navbar

--- components/common/navbar.js
+++ components/common/navbar.js
@@ -32,7 +32,7 @@ export default function Navbar() {
 		<header className='w-full relative z-50 bg-base-100 p-5 pb-0 container mx-auto md:mb-5 flex justify-between items-center'>
 			<a
 				aria-label='landing page template'
-				className='flex bg-green400 items-center w-2/5'
+				className='flex bg-green400 items-center w-full'
 				title='landing page template'
 				href={`/en`}
 			>
@@ -43,22 +43,22 @@ export default function Navbar() {
 					className='rotator transition-all hover:scale-110 w-6 md:w-14 h-6 md:h-14'
 					alt='logo'
 				></Image>
-				<div className='flex flex-col h14 justify-around  text-base-content/80'>
+				<div className='flex flex-col h14 justify-around   text-base-content/80'>
 				<h2 className='ml-3 font-bold text-[22px] '>EXPO ESSENTIALS</h2>
 				<p className='ml-3 text-[14px]'>BOOTH CONSTRUCTION</p>
 				</div>
 			</a>
 
-			<ul className='w-3/5 px-5 font-medium hidden md:flex flex-nowrap items-center bg-red300 justify-around'>
+			<ul className='w-full px-5 gap-9 font-medium hidden md:flex flex-nowrap items-center bg-red300 justify-around'>
 				{linkList.map((link, index) => {
 					return (
 						<li
 							key={index}
-							className='group py-3 text-center'
+							className='group py-3  text-center'
 						>
 							<a
 								aria-label={link.name}
-								className='group relative'
+								className='group relative '
 								title={link.name}
 								href={`/${langName}${link.url}`}
 							>
@@ -70,7 +70,7 @@ export default function Navbar() {
 				})}
 			</ul>
 
-			<div className='md:w-1/5 flex items-center justify-end gap-2'>
+			<div className='md:w-full flex items-center justify-end gap-2'>
 				
 				{/* <ThemeToggle /> */}
 				{/* <LangSwitch /> */}
